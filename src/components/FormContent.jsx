185065a6289960318fc5a244eee5bd5bda008afc
Mohamed Shafran <shafransheikh@gmail.com>
2024-09-07 12:01:17 +0530

    add logic for the formcontent file

--- src/components/FormContent.jsx
+++ src/components/FormContent.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function FormContent(props) {
+function FormContent() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await axios.post('http://localhost:3000/api/', {username, password});
+      if(response.data.success){
+        console.log("Login Successful");
+      }else{
+        console.log("An error occured");
+        
+      }
+    }catch(error){
+      console.log(error);
+    }
+  
+  }
   return (
     <div className="login-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder="Enter your username" />
+          <input type="text" name="username" placeholder="Enter your username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
             type="password"
-            id="password"
-            placeholder="Enter your password"
+            name="password"
+            placeholder="Enter your password"value={password} onChange={(e)=>setPassword(e.target.value)}
           />
         </div>
         <button type="submit">Login</button>
